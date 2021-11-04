840b106f15eaf6a2ae943a98b7bdf90a23b4f744
carinebecker <caarine.m@gmail.com>
2021-11-03 22:47:27 -0300

    feat: cria e organiza componentes

---
+++ src/components/Form.jsx
@@ -0,0 +1,7 @@
+export function Form() {
+    return (
+        <>
+        <p>form</p>
+        </>
+    )
+}
---
+++ src/components/Table.jsx
@@ -0,0 +1,7 @@
+export function Table() {
+    return (
+        <>
+        <p>table</p>
+        </>
+    )
+}
