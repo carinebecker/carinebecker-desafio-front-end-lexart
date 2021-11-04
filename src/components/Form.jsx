5b524e025b5bbd43ae214142e8b940bf16478e51
carinebecker <caarine.m@gmail.com>
2021-11-04 10:04:55 -0300

    feat: cria base do formulário

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,44 @@
 export function Form() {
     return (
         <>
-        <p>form</p>
+        <form method="POST">
+            <label>
+                _Id
+                <input type="text" />
+            </label>
+            <label>
+                Quantity
+                <input type="number" min="1" step="1"/>
+            </label>
+            <label>
+                Price
+                <input type="number" min="1" step="any" />
+            </label>
+            <label>
+                Product
+                <select name="product" id="product">
+                    <option value="default">default_product</option>
+                </select>
+            </label>
+            <label>
+                Client
+                <select name="client" id="client">
+                    <option value="default">default_client</option>
+                </select>
+            </label>
+            <label>
+                Active
+                <label htmlFor="active_yes">
+                    <input type="radio" name="active" id="active_yes" />
+                    yes
+                </label>
+                <label htmlFor="active_no">
+                    <input type="radio" name="active" id="active_no" />
+                    no
+                </label>
+            </label>
+            <button type="submit">Register</button>
+        </form>
         </>
     )
 }
