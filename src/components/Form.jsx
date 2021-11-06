a4966dcaacee2863ee8880b9f1f9476aa1e94e75
carinebecker <caarine.m@gmail.com>
2021-11-05 22:21:33 -0300

    feat: configura estados e fetch

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,44 +1,95 @@
+import { useState } from "react";
+
 export function Form() {
-    return (
-        <>
-        <form method="POST">
-            <label>
-                _Id
-                <input type="text" />
-            </label>
-            <label>
-                Quantity
-                <input type="number" min="1" step="1"/>
-            </label>
-            <label>
-                Price
-                <input type="number" min="1" step="any" />
-            </label>
-            <label>
-                Product
-                <select name="product" id="product">
-                    <option value="default">default_product</option>
-                </select>
-            </label>
-            <label>
-                Client
-                <select name="client" id="client">
-                    <option value="default">default_client</option>
-                </select>
-            </label>
-            <label>
-                Active
-                <label htmlFor="active_yes">
-                    <input type="radio" name="active" id="active_yes" />
-                    yes
-                </label>
-                <label htmlFor="active_no">
-                    <input type="radio" name="active" id="active_no" />
-                    no
-                </label>
-            </label>
-            <button type="submit">Register</button>
-        </form>
-        </>
-    )
+	const [payload, setPayload] = useState({
+		product: "default",
+		client: "default",
+	});
+
+	function handleChange({ target }) {
+		const { name, value } = target;
+		setPayload({ ...payload, [name]: value });
+	}
+
+	function handlePriceChange({ target }) {
+		const { name, value } = target;
+		setPayload({ ...payload, [name]: parseFloat(value).toFixed(2) });
+	}
+
+	function handleSubmit(e) {
+		e.preventDefault();
+		fetch("/api/bf3c6a69da854c03926955a6c2051f0f/abm-stock", {
+			method: "POST",
+			headers: { "content-type": "application/json"},
+			body: JSON.stringify(payload),
+		})
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+        .catch((err) => console.log(err));
+	}
+
+	return (
+		<form onSubmit={ handleSubmit }>
+			<label>
+				Quantity
+				<input
+					type="number"
+					min="1"
+					step="1"
+					name="quantity"
+					onChange={handleChange}
+					required
+				/>
+			</label>
+			<label>
+				Price
+				<input
+					type="number"
+					name="price"
+					min="1"
+					step="0.01"
+					placeholder="0.00"
+					onChange={ handlePriceChange }
+					required
+				/>
+			</label>
+			<label>
+				Product
+				<select name="product" id="product" onChange={ handleChange }>
+					<option value="default">default_product</option>
+				</select>
+			</label>
+			<label>
+				Client
+				<select name="client" id="client" onChange={ handleChange }>
+					<option value="default">default_client</option>
+				</select>
+			</label>
+			<label>
+				Active
+				<label htmlFor="active_yes">
+					<input
+						type="radio"
+						name="active"
+						id="active_yes"
+						value="yes"
+						onChange={ handleChange }
+						required
+					/>
+					yes
+				</label>
+				<label htmlFor="active_no">
+					<input
+						type="radio"
+						name="active"
+						id="active_no"
+						value="no"
+						onChange={ handleChange }
+					/>
+					no
+				</label>
+			</label>
+			<button>Register</button>
+		</form>
+	);
 }
